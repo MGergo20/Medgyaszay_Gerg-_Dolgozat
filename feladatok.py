42a4a3e5b71050ee0da6cbe4f2c3ecee5f0763f5
MGergo20 <medgyaszayg3@gmail.com>
2024-11-07 08:58:03 +0100

    2-es feladat kész

--- feladatok.py
+++ feladatok.py
@@ -1,5 +1,27 @@
+import random
+
 def feladat1():
     szam:int=int(input("Kérek egy páros számot!"))
     while szam%2!=0:
-        szam:int=int(input("Kérek egy páros számot!"))
+        szam:int=int(input("Ez nem páros! PÁROS számot kérek!"))
     print(szam)
+
+
+def feladat2():
+    lista=[]
+    i=0
+    while i<13:
+        szam2:int=int(random.random()*(141)+10)
+        lista.append(szam2)
+        i+=1
+    return lista
+
+def feladat2_szamolas(lista):
+    i:int=0
+    db:int=0
+    while(i<len(lista)):
+        if(lista[i]%3==0):
+            db+=1
+        i+=1
+    return db
+    
--- main.py
+++ main.py
@@ -1,4 +1,10 @@
 import feladatok
 
-feladatok.feladat1()
-print("Kettes feladat")
+'''feladatok.feladat1()
+print("Kettes feladat")'''
+
+lista=feladatok.feladat2()
+print(lista)
+
+db=feladatok.feladat2_szamolas(lista)
+print(f"A számok között {db} db 3-mal osztható van!")
